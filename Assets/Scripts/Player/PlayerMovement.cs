d1d6913528a3a4681371dcf4f857a5279327e932
GDWR <gregory.dwr@gmail.com>
2020-05-04 12:55:50 +0100

    Added Bow Character
    You can swap class using Unity Editor, PlayerStats script.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,34 +6,21 @@ using Random = System.Random;
 
 public class PlayerMovement : MonoBehaviour
 {
-	
 	#region Public Properties
 	public CharacterController controller;
 	public Animator animator;
-	public float moveSpeed = 30;
-	public int damage = 0;
+	public bool canAct = true; // locks controls if false.
 	#endregion
 	
 	#region Private Properties
-	private bool canAct = true; // locks controls if false.
+	private float moveSpeed = PlayerStats.moveSpeed;
 	private Vector3 _playerMovement;
-	private int idleTimer;
 	#endregion
 	
 	void Update()
 	{
 		if (canAct)
 		{
-			if (Input.GetMouseButtonDown(0))
-			{
-				//light attack
-				StartCoroutine(LightAttackCoroutine());
-			}
-			else if (Input.GetMouseButtonDown(1))
-			{
-				//heavy attack
-				StartCoroutine(HeavyAttackCoroutine());
-			}
 			MovePlayer();
 		}
 	}
@@ -47,28 +34,18 @@ public class PlayerMovement : MonoBehaviour
 		if (horizontal != 0 || vertical != 0)
 		{
 			if (vertical == 0)
-				animator.SetInteger("Strafe", (int) horizontal); 
+				animator.SetInteger("Horizontal", (int) horizontal); 
 			else
-				animator.SetInteger("Direction", (int) vertical);
+				animator.SetInteger("Vertical", (int) vertical);
 			if (vertical == -1)
 			{
 				speed *= 0.5f;
 			}
-			animator.SetTrigger("ExitIdle");
-			animator.SetInteger("Speed", 1);
 		}
 		else
 		{
-			animator.SetInteger("Strafe", 0);
-			animator.SetInteger("Direction", 0);
-
-			animator.ResetTrigger("ExitIdle");
-			animator.SetInteger("Speed", 0);
-			idleTimer++;
-			if (idleTimer > 5000)
-			{
-				RandomIdleAnimation();
-			}
+			animator.SetInteger("Horizontal", 0);
+			animator.SetInteger("Vertical", 0);
 		}
 		
 		var move = transform.right * horizontal + transform.forward * vertical;
@@ -86,37 +63,9 @@ public class PlayerMovement : MonoBehaviour
 		canAct = true;
 	}
 
-	private void RandomIdleAnimation()
+	public bool IsMoving()
 	{
-		animator.SetInteger("Idle", new Random().Next(1,5));
-		Debug.Log("Random Idle Animation");
-		animator.SetInteger("Idle", 0);
-
-		idleTimer = 0;
-	}
-
-	IEnumerator LightAttackCoroutine()
-	{
-		canAct = false;
-		damage = 10;
-		animator.SetInteger("Attack", 1);
-		yield return new WaitForSeconds(1f);
-		animator.SetInteger("Attack", 0);
-		damage = 0;
-		canAct = true;
-	}
-	
-	IEnumerator HeavyAttackCoroutine()
-	{
-		canAct = false;
-		damage = 25;
-		animator.SetInteger("Attack", 2);
-		var move = transform.right * 0 + transform.forward * 2;
-		controller.Move(move * (moveSpeed * Time.deltaTime));
-		yield return new WaitForSeconds(1.5f);
-		animator.SetInteger("Attack", 0);
-		damage = 0;
-		canAct = true;
+		return animator.GetInteger("Horizontal") == 0  && animator.GetInteger("Vertical") == 0;
 	}
 
 }
--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,19 +7,63 @@ public class PlayerStats : MonoBehaviour
 {
     #region public properties
 
+    public PlayerType playerType;
     public int health;
     public int maxHealth;
-
+    public static float moveSpeed = 5;
+    public Animator animator;
+    public GameObject Player;
+    public GameObject LeftHand;
+    public GameObject RightHand;
     #endregion
-    // Start is called before the first frame update
-    void Start()
+    
+    public enum PlayerType
     {
-        
+        GreatSword,
+        Bow,
+        NoWeapon
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void Start()
     {
+        //TODO: make playerType add weapon to hand (Attach to appropriate limb to allow for it to move correctly)
+        // BowClass
+        // Wooden bow -> attach LeftHand -> transform (0.129, -0.035, 0.307) -> rotation(90, -90, 90)
+        // Arrow -> attach RightHand -> transform (0.206, -0.025, 0.019) -> rotation(90, -90, -180)
         
+        // GreatSwordClass
+        // Great Sword -> attach RightHand -> transform(0.055, -0.036, -0.054) -> rotation (0, 30, 0)
+        switch (playerType) 
+        {
+            case PlayerType.NoWeapon:
+                break;
+            
+            case PlayerType.GreatSword:
+                animator.runtimeAnimatorController = Resources.Load("AnimationControllers/GreatSwordAnimationController") as RuntimeAnimatorController;
+                var greatSwordObj = Resources.Load("PlayerWeapons/GreatSword") as GameObject;
+                greatSwordObj.transform.position = new Vector3(0.055f, -0.036f, -0.054f);
+                greatSwordObj.transform.rotation = Quaternion.Euler(0, 30, 0);
+                Instantiate(greatSwordObj, RightHand.transform);
+                Player.AddComponent<GreatSword>();
+                break;
+            
+            case PlayerType.Bow:
+                animator.runtimeAnimatorController = Resources.Load("AnimationControllers/BowAnimationController") as RuntimeAnimatorController;
+                var bowObj = Resources.Load("PlayerWeapons/Bow") as GameObject;
+                var ArrowObj = Resources.Load("PlayerWeapons/Arrow") as GameObject;
+                bowObj.transform.position = new Vector3(0.129f, -0.035f, 0.307f);
+                ArrowObj.transform.position = new Vector3(0.206f, -0.025f, 0.019f);
+                bowObj.transform.rotation = Quaternion.Euler(90, -90, 90);
+                ArrowObj.transform.rotation = Quaternion.Euler(90, -90, -180);
+                Instantiate(bowObj, LeftHand.transform);
+                Instantiate(ArrowObj, RightHand.transform);
+                Player.AddComponent<Bow>();
+                break;
+            
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
