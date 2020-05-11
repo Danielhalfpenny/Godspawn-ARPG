03f5310981603bfe968724cb49d585acf65ba005
GDWR <gregory.dwr@gmail.com>
2020-05-11 15:08:46 +0100

    Added Basic StartMenu
    StaticVars script is used to pass vars across scenes

---
+++ Assets/StaticVars.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticVars : MonoBehaviour
+{
+    // To pass vars across scenes
+    
+    public static PlayerStats.PlayerType playerType;
+
+    public void SetPlayerType(PlayerStats.PlayerType newPlayerType)
+    {
+        playerType = newPlayerType;
+    }
+    
+}
